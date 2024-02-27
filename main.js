1fc62fbea3f35a7971616ddae04c8b1c1b9336e8
EJAZ2024AI <161510024+EJAZ2024AI@users.noreply.github.com>
2024-02-27 10:47:45 -0800

    EXERCISE 2
    TYPESCRIPT ASSIGNMENT

---
+++ main.js
@@ -0,0 +1,4 @@
+//let personName: string = "Eric";
+//console.log(`Hello ${personName}, would you like to learn some Python today?`);
+var personName = "Ejaz";
+console.log("Hello ".concat(personName, ",would you like to learn some typescript today?"));
